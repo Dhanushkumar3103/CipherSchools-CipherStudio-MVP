4a4b7152ad3d84f4f481d10654828088cf3d27c4
Dhanush Paladi <118897753+Dhanushkumar3103@users.noreply.github.com>
2025-10-22 21:55:53 +0530

    Initialize React application in index.js

---
+++ 6️⃣ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CipherStudioApp from "./CipherStudioApp";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<CipherStudioApp />);
